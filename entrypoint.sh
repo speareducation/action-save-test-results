e287606529b6b5c648899554c23adfca036f6975
Jason Wright <jwright@speareducation.com>
2020-07-09 13:17:34 -0700

    renamed targetDir to sourceDir

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,12 +6,12 @@ COMMIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 S3_BUCKET="s3://$INPUT_S3_BUCKET"
 S3_BRANCH_PATH="$S3_BUCKET/$REPO_SLUG/$COMMIT_BRANCH"
 
-[ -d "$INPUT_TARGETDIR/databases" ] && rm -rf "$INPUT_TARGETDIR/databases"
-git log -1 > "$INPUT_TARGETDIR/git-status.txt"
+[ -d "$INPUT_SOURCEDIR/databases" ] && rm -rf "$INPUT_SOURCEDIR/databases"
+git log -1 > "$INPUT_SOURCEDIR/git-status.txt"
 
-echo "$S3_BRANCH_PATH/$COMMIT_HASH" > "$INPUT_TARGETDIR/s3-path.txt"
+echo "$S3_BRANCH_PATH/$COMMIT_HASH" > "$INPUT_SOURCEDIR/s3-path.txt"
 echo "Uploading to $S3_BRANCH_PATH/$COMMIT_HASH"
-aws s3 sync --only-show-errors --delete --acl=public-read "./$INPUT_TARGETDIR" "$S3_BRANCH_PATH/$COMMIT_HASH" && \
+aws s3 sync --only-show-errors --delete --acl=public-read "./$INPUT_SOURCEDIR" "$S3_BRANCH_PATH/$COMMIT_HASH" && \
 
 # releases/sandbox/gl-123        -> releases/sandbox/gl-123/latest
 # releases/production/2020-01-01 -> releases/production/latest
